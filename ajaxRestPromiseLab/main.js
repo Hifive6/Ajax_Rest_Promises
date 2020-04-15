d19822e5d032193841f4e677eaedad2fcb6cb690
David Juarez <djuar05@gmail.com>
2020-04-15 14:52:26 -0400

    setting text with button

--- ajaxRestPromiseLab/main.js
+++ ajaxRestPromiseLab/main.js
@@ -1,9 +1,16 @@
+
+
 document.addEventListener('DomContentLoaded', function(){
 
-    //Add you code below this line
-        document.getElementById('getMessage').onclick = function(){
+    //Add you code below this line for handle Click Event
+    document.getElementById('getMessage').onclick = function(){
+            console.log()
+            //Add your code above this line
+                
+            //Add your code below this ine for Change Text with Click Event
+            document.getElementsByClassName('message')[0].textContent="Here is the message";
 
+            //Add your code above this line
         }
-    //Add your code above this line
 
 })
