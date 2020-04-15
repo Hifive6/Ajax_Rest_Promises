b3dbcc1f4fdb8204600418329bf1fbeea1a527a9
David Juarez <djuar05@gmail.com>
2020-04-15 14:34:42 -0400

    Handle click events

---
+++ ajaxRestPromiseLab/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DomContentLoaded', function(){
+
+    //Add you code below this line
+        document.getElementById('getMessage').onclick = function(){
+
+        }
+    //Add your code above this line
+
+})
